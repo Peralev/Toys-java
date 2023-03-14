8974b35fd806065e34141389ada71a9f4f9e8f21
Elau <lol1.com@mail.ru>
2023-03-14 23:24:56 +0300

    Создали класс Toy

--- Toys.java
+++ Toys.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
+class Toy {
+
+    public String name;
+    public int id;
+    public int weight;
+    public int count; 
+
+    public Toy(String name, int id, int weight) {
+        this.name = name;
+        this.id = id;
+        this.weight = weight;
+        this.count = 0;
+    }   
+}
 
 public class Toys {
     
